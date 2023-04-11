4d4445fa5d5fdc8dce391998fee0609f231d262b
Vincent Lalanne <v.lalanne@fugu.fr>
2023-04-11 13:43:38 +0200

    fix: 0.4.4 notice message typo

--- version-notices.ts
+++ version-notices.ts
@@ -20,7 +20,7 @@ const versionNotices: ObjectOf<Notice> = {
     nous procédons régulièrement à des travaux sur le site et malgré nos efforts, 
     il arrive que des bugs réussissent à passer à travers les mailles de nos filets.</p>
     <p class='my-2'>Nina.fm est à nouveau pleinement opérationnelle, mais vos messages 
-    seront toujours les bienvenues dans le cas contraire !</p>
+    seront toujours les bienvenus dans le cas contraire !</p>
     <p>Nous vous prions de bien vouloir nous excuser pour la gêne occasionnée 
     et vous remercions pour votre patience et votre soutien indéfectible.</p>
     <p class='signature'>L'équipe Nina.fm</p>`,
