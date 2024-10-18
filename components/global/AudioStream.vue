a2e97df9b66cbc36ecf3b1fadb1e998f7fb3a096
Vincent Lalanne <v.lalanne@fugu.fr>
2024-10-18 11:22:24 +0200

    fix: add v-once to <audio>

--- components/global/AudioStream.vue
+++ components/global/AudioStream.vue
@@ -3,7 +3,7 @@ const { streamRef } = useAudioStoreRefs()
 </script>
 
 <template>
-  <audio ref="streamRef" src="https://flux.nina.fm/nina.mp3" type="audio/mpeg" autoplay>
+  <audio v-once ref="streamRef" src="https://flux.nina.fm/nina.mp3" type="audio/mpeg" autoplay>
     Votre navigateur est un vieux machin dépassé. Il ne supporte pas la musique, ce qui est un peu con quand on veut
     écouter la radio.
   </audio>
