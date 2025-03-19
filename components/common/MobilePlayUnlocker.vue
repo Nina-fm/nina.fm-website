12726a61d4c71350e536cbb4ea381e0f91604c18
Vincent Lalanne <v.lalanne@fugu.fr>
2025-03-19 14:39:46 +0100

    feat: add mobile play unlocker

---
+++ components/common/MobilePlayUnlocker.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const { unlock } = useAudioStore()
+const { isLocked } = useAudioStoreRefs()
+</script>
+
+<template>
+  <div
+    v-if="isLocked"
+    class="absolute inset-0 z-50 bg-background/60 flex flex-col items-center justify-center"
+    @click="unlock"
+  >
+    <NinaIcon icon="play_circle_outline" class="text-primary" :style="{ fontSize: '60vmin' }" />
+  </div>
+</template>
+
+<style></style>
