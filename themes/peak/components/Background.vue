8424819419e33b6417a5ccb866a3c3225486067d
Vincent Lalanne <v.lalanne@fugu.fr>
2025-03-20 14:22:05 +0100

    style(peak): fix background mask max sizes

--- themes/peak/components/Background.vue
+++ themes/peak/components/Background.vue
@@ -1,23 +1,18 @@
 <script lang="ts" setup>
-const maskLight = (await import(`../assets/images/mask.png`)).default;
-const maskDark = (await import(`../assets/images/mask-night.png`)).default;
-const imgLight = (await import(`../assets/images/background.jpg`)).default;
-const imgDark = (await import(`../assets/images/background-night.jpg`)).default;
-const animatedLight = (await import(`../assets/images/animated-background.gif`))
-  .default;
-const animatedDark = (
-  await import(`../assets/images/animated-background-night.gif`)
-).default;
+const maskLight = (await import(`../assets/images/mask.png`)).default
+const maskDark = (await import(`../assets/images/mask-night.png`)).default
+const imgLight = (await import(`../assets/images/background.jpg`)).default
+const imgDark = (await import(`../assets/images/background-night.jpg`)).default
+const animatedLight = (await import(`../assets/images/animated-background.gif`)).default
+const animatedDark = (await import(`../assets/images/animated-background-night.gif`)).default
 
-const { isDarkModeActive } = useThemeStoreRefs();
-const { isPlaying } = useAudioStoreRefs();
+const { isDarkModeActive } = useThemeStoreRefs()
+const { isPlaying } = useAudioStoreRefs()
 
-const mask = computed(() => (isDarkModeActive.value ? maskDark : maskLight));
-const img = computed(() => (isDarkModeActive.value ? imgDark : imgLight));
-const animated = computed(() =>
-  isDarkModeActive.value ? animatedDark : animatedLight
-);
-const bg = computed(() => (isPlaying.value ? animated : img));
+const mask = computed(() => (isDarkModeActive.value ? maskDark : maskLight))
+const img = computed(() => (isDarkModeActive.value ? imgDark : imgLight))
+const animated = computed(() => (isDarkModeActive.value ? animatedDark : animatedLight))
+const bg = computed(() => (isPlaying.value ? animated : img))
 </script>
 
 <template>
@@ -26,6 +21,6 @@ const bg = computed(() => (isPlaying.value ? animated : img));
       class="w-full h-full bg-[left_15%_center] md:bg-left bg-cover bg-no-repeat"
       :style="{ backgroundImage: `url(${bg.value})` }"
     />
-    <img :src="mask" class="absolute bottom-0 left-0" />
+    <img :src="mask" class="absolute bottom-0 left-0 max-h-[65%] max-w-[105%]" />
   </div>
 </template>
