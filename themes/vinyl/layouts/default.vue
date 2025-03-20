9954f042fa261397ad48f6658aca0d65023d5bb0
Vincent Lalanne <v.lalanne@fugu.fr>
2025-03-20 14:22:51 +0100

    style: reduce global margin on mobile for both themes

--- themes/vinyl/layouts/default.vue
+++ themes/vinyl/layouts/default.vue
@@ -3,7 +3,7 @@ const { isContentOpen } = useVinylThemeStoreRefs()
 </script>
 
 <template>
-  <div class="absolute inset-5 overflow-hidden rounded">
+  <div class="absolute inset-4 md:inset-5 overflow-hidden rounded">
     <slot />
     <Controls :class="cn('text-primary-foreground', { 'text-primary hidden md:flex': isContentOpen })" />
   </div>
