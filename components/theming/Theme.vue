0a506203ff96c7d34bfd11f72330dc2b2c5526f0
Vincent Lalanne <v.lalanne@fugu.fr>
2025-03-22 19:22:39 +0100

    fix: try to fix again

--- components/theming/Theme.vue
+++ components/theming/Theme.vue
@@ -3,12 +3,12 @@
     name: ThemeKey
   }>()
   const { name } = toRefs(props)
-  const componentRef = ref<ThemeKey | null>(null)
+  const componentRef = ref<string | null>(null)
 
   const theme = computed(() => defineAsyncComponent(() => import(`../../themes/${name.value}/theme.vue`)))
 
   watch(name, (value) => {
-    componentRef.value = value
+    componentRef.value = `${value}-${Date.now()}`
   })
 </script>
 
