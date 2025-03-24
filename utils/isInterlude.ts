8856d6b3b492a9d8242278861ed4e24f73b983a1
Vincent Lalanne <v.lalanne@fugu.fr>
2025-03-24 09:13:09 +0100

    feat: add more interlude words

--- utils/isInterlude.ts
+++ utils/isInterlude.ts
@@ -1,4 +1,4 @@
-export const interludeWords = ['interlude', 'intro', 'outro']
+export const interludeWords = ['interlude', 'intro', 'outro', 'jingle', 'annonce']
 
 export default function isInterlude(track: Track) {
   const regex = new RegExp(interludeWords.join('|'), 'i')
