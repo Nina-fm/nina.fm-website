4a9f8b6d32f3d55b93b71ae20123d0208bf19006
Vincent Lalanne <v.lalanne@fugu.fr>
2025-11-11 15:54:58 +0100

    fix: better check for interludes

--- utils/isInterlude.ts
+++ utils/isInterlude.ts
@@ -1,7 +1,7 @@
 export const interludeWords = ['interlude', 'introlude', 'outrolude', 'outlude', 'intro', 'outro', 'jingle', 'annonce']
 
 export default function isInterlude(track: Track) {
-  if (!track.title) return false
+  if (!track.artist) return false
 
-  return interludeWords.includes(track.title.toLowerCase())
+  return interludeWords.includes(track.artist.toLowerCase())
 }
